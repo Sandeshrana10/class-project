b997d5b083d45b368a1b99edad0181cf605514ff
Sandeshrana10 <sandeshrana211@gmail.com>
2024-08-10 18:53:27 +0545

    first commit

--- index.js
+++ index.js
@@ -22,52 +22,98 @@
 
 // Sum(8, 9, DisplaySum);
 
-//Promise
+// callback
 
-// const res = new Promise((resolve, rejected) => {
-//     let success = true;
-//     if (success) {
+// function Sum(a,b,cb){
+//     let ans=a+b;
+//     cb(ans);
+// }
+
+// function DisplaySum(result){
+//     const h1=document.createElement("h1");
+//     h1.innerHTML=`The sum of number is:$(result)`;
+//     document.body.append(h1);
+// }
+
+// Sum(8,9,DisplaySum);
+
+// Promise
+
+// const res=new Promise((resolve,rejected)=>{
+//     let success=true;
+//     if(success){
 //         resolve("Data Fetched");
 //     } else{
 //         rejected("Server Error");
 //     }
-//     resolve("Promise Fulfilled");});
-//     res.then((data) => console.log(data)).catch((error) => console.log
-//     (error));
+// });
+// res.then((data)=>console.log(data)).catch((error)=>console.lof(error));
 
-//     res.then((data))=>console.log((data)).catch((error)) => console.log((error));
-
-//CatFact
-
-// const fetchedData = ()=>{
-//     fetch(`https://catfact.ninja/fact`)
-//     .then((res) => res.json())
-//     .then((data) => {
-//         const h1 = document.createElement("h1");
-//         h1.innerHTML = data.fact;
-//         document.body.append(h1);
+// CatFact
+// const fetchedData=()=>{
+//     fetch(https://catfact.ninja/fact)
+//     .then((res)=>res.json())
+//     .then((data)=>{
+//      const h1=document.createElement("h1");
+//      h1.innerHTML=data.fact;
+//      document.body.append(h1);
 //     })
-//     .catch((error) => console.log(error));
+//     .catch((error)=>console.log(error));
 // };
 
 // fetchedData();
 
+// Async and Await
+// const fetchedData=async()=>{
+//     try{
+//     const res=await fetch(https://catfact.ninja/fact);
+//     const data=await res.json();
+//     const h1=document.createElement("h1");
+//     h1.innerHTML=data.fact;
+//     document.body.append(h1);
+// } catch (error){
+//     console.log(error);
+// }
+// };
 
-//Async And Await
+// fetchedData();
 
-const fetchedData = async()=>{
-    try {
-    const res = await fetch(`https://catfact.ninja/fact`);
-    const data = await res.json();
-    const h1 = document.createElement("h1");
-    h1.innerHTML = data.fact;
-    document.body.append(h1);
-} catch (error){
-    console.log(error);
-}
+// console.log("Hello World!");
 
+const temperatureField=document.querySelector(".weather1");
+const cityField=document.querySelector(".weather2 p");
+const dateField=document.querySelector(".weather2 span");
+const emojiField=document.querySelector(".weather3 img");
+const weatherField=document.querySelector(".weather3 span");
+const form=document.querySelector("form");
+const input=document.querySelector(".searchField");
+let target="Kathmandu";
+const fetchedData=async()=>{
+    const res=await fetch(
+        https://api.weatherapi.com/v1/current.json?key=75d949d65efa4e23a5e21917240908&q=${target}
+    );
+    const data=await res.json();
+
+    console.log(data);
+    const {
+        current:{
+            temp_c,
+            condition:{text,icon}},
+        location:{name,localtime},
+    }=data;
+    
+    temperatureField.innerHTML=`${temp_c}&degC`;
+    cityField.innerHTML=name;
+    emojiField.src=icon;
+    weatherField.innerHTML=text;
+
+    const currentTime=localtime.split(" ")[1];
+    const currentDate=localtime.split(" ")[0];
+
+    dateField.innerHTML=`${currentTime} ${currentDate}`;
 };
 
-fetchedData()
+fetchedData();
+
 
 console.log("Hello World");
